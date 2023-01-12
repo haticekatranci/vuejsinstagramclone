560973bdfde1adb869ebe527f14191a7c401f5e1
hatice <haticekatranci@yandex.com>
2023-01-12 15:08:22 +0300

    add css

---
+++ instagramclone/src/views/HomeView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+
+    <CustomText>Merhaba</CustomText>
+    <CustomText tag="p" class="a1">Merhaba</CustomText>
+    <CustomText tag="p">Merhaba</CustomText>
+    <CustomText tag="p" size="xsmall">Merhaba</CustomText>
+    <CustomText tag="p" size="xxsmall">Merhaba</CustomText>
+
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import CustomText from "../components/CustomText.vue"
+
+
+export default {
+  name: "HomeView",
+  components: {
+    CustomText
+  }
+};
+</script>
+
+<style scoped>
+.a1 {
+  color: rgb(var(--a72));
+}
+
+</style>
