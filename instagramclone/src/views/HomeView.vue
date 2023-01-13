06cd0f2fe4ae4051e28422e26292d7817f077b05
hatice <haticekatranci@yandex.com>
2023-01-13 17:10:24 +0300

    add svg

--- instagramclone/src/views/HomeView.vue
+++ instagramclone/src/views/HomeView.vue
@@ -6,6 +6,10 @@
     <CustomText tag="p">Merhaba</CustomText>
     <CustomText tag="p" size="xsmall">Merhaba</CustomText>
     <CustomText tag="p" size="xxsmall">Merhaba</CustomText>
+    <IconHome></IconHome>
+    <svg width="100" height="100">
+      <circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" />
+    </svg>
 
 
   </div>
@@ -14,12 +18,14 @@
 <script>
 // @ is an alias to /src
 import CustomText from "../components/CustomText.vue"
+import IconHome from '../icons/home.svg'
 
 
 export default {
   name: "HomeView",
   components: {
-    CustomText
+    CustomText,
+    IconHome
   }
 };
 </script>
